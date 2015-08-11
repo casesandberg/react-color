9606927f0a6167b4a9099dcc0675bcd096eeef08
case <case@casesandberg.com>
2015-08-11 05:04:49 -0400

    Expand Button

--- src/components/chrome/ChromeFields.jsx
+++ src/components/chrome/ChromeFields.jsx
@@ -28,6 +28,11 @@ class ChromeFields extends ReactCSS.Component {
           width: '32px',
           textAlign: 'right',
         },
+        icon: {
+          marginRight: '-4px',
+          marginTop: '12px',
+          cursor: 'pointer',
+        },
         Input: {
           style: {
             input: {
@@ -66,7 +71,11 @@ class ChromeFields extends ReactCSS.Component {
           <EditableInput is="Input" label="hex" value={ '#aeee00' } />
         </div>
         <div is="toggle">
-          |
+          <div is="icon">
+            <svg style={{ width:'24px', height:'24px', }} viewBox="0 0 24 24">
+              <path fill="#333" d="M12,18.17L8.83,15L7.42,16.41L12,21L16.59,16.41L15.17,15M12,5.83L15.17,9L16.58,7.59L12,3L7.41,7.59L8.83,9L12,5.83Z" />
+            </svg>
+          </div>
         </div>
       </div>
     );
