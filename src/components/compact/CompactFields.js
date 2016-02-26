f5ec5dd56b0b6c42c73a5b0bd1aa73f57036f646
case <case@casesandberg.com>
2016-02-26 17:57:55 -0600

    Compact Source

--- src/components/compact/CompactFields.js
+++ src/components/compact/CompactFields.js
@@ -90,9 +90,13 @@ export class CompactColor extends ReactCSS.Component {
         r: data.r || this.props.rgb.r,
         g: data.g || this.props.rgb.g,
         b: data.b || this.props.rgb.b,
+        source: 'rgb',
       })
     } else {
-      this.props.onChange(data)
+      this.props.onChange({
+        hex: data,
+        source: 'hex',
+      })
     }
   }
 
