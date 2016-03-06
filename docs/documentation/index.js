07548e28de70e01ace460f7cff6a35c9fb03de4a
case <case@casesandberg.com>
2016-03-06 16:22:12 -0600

    Remove Position Documentation

--- docs/documentation/index.js
+++ docs/documentation/index.js
@@ -10,12 +10,8 @@ module.exports = {
   '03.02-onChange': require('./03.02-onChange.md'),
   '03.03-onChangeComplete': require('./03.03-onChangeComplete.md'),
   '03.04-color': require('./03.04-color.md'),
-  '03.05-display': require('./03.05-display.md'),
-  '03.06-onClose': require('./03.06-onClose.md'),
-  '03.07-position': require('./03.07-position.md'),
-  '03.08-positionCSS': require('./03.08-positionCSS.md'),
   '04-create': require('./04-create.md'),
   '04.01-parent': require('./04.01-parent.md'),
   '04.02-helpers': require('./04.02-helpers.md'),
   '05-examples': require('./05-examples.md'),
-};
+}
