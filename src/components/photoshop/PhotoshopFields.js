a2a465b5d9e4bc1efd38f52955192d1b6651b904
Brad Buchanan <bradley.c.buchanan@gmail.com>
2016-05-10 23:53:08 -0700

    fix and cover photoshop-style picker

--- src/components/photoshop/PhotoshopFields.js
+++ src/components/photoshop/PhotoshopFields.js
@@ -125,7 +125,7 @@ export class PhotoshopPicker extends ReactCSS.Component {
         <EditableInput is="Input" label="g" value={ this.props.rgb.g } onChange={ this.handleChange }/>
         <EditableInput is="Input" label="b" value={ this.props.rgb.b } onChange={ this.handleChange }/>
         <div is="divider" />
-        <EditableInput is="Hex" label="#" value={ this.props.hex } onChange={ this.handleChange }/>
+        <EditableInput is="Hex" label="#" value={ this.props.hex.replace('#', '') } onChange={ this.handleChange }/>
         <div is="fieldSymbols">
           <div is="symbol">°</div>
           <div is="symbol">%</div>
