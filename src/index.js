0af1bbf59069e2c29d4751671ebac5d44d24f3f0
case <case@casesandberg.com>
2016-09-04 17:22:13 -0700

    GithubPicker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as BlockPicker } from './components/block/Block'
 export { default as ChromePicker } from './components/chrome/Chrome'
 export { default as CompactPicker } from './components/compact/Compact'
+export { default as GithubPicker } from './components/github/Github'
 export { default as MaterialPicker } from './components/material/Material'
 export { default as PhotoshopPicker } from './components/photoshop/Photoshop'
 export { default as SketchPicker } from './components/sketch/Sketch'
