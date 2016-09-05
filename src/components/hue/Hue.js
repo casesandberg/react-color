49da732073484eefd19efc09de2b2866229fad04
case <case@casesandberg.com>
2016-09-04 21:29:07 -0700

    Hue Picker

---
+++ src/components/hue/Hue.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import reactCSS from 'reactcss'
+
+import { ColorWrap, Hue } from '../common'
+import HuePointer from './HuePointer'
+
+export const HuePicker = (props) => {
+  const styles = reactCSS({
+    'default': {
+      hue: {
+        position: 'relative',
+        width: props.width,
+        height: props.height,
+      },
+      Hue: {
+        radius: '2px',
+      },
+    },
+  })
+
+  const handleChange = (data, e) => {
+    props.onChange && props.onChange(data, e)
+  }
+
+  return (
+    <div style={ styles.hue }>
+      <Hue
+        { ...styles.Hue }
+        { ...props }
+        pointer={ HuePointer }
+        onChange={ handleChange }
+      />
+    </div>
+  )
+}
+
+HuePicker.defaultProps = {
+  width: '316px',
+  height: '16px',
+}
+
+export default ColorWrap(HuePicker)
