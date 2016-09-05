0ae22090d936efce196e659ce7f12f7d66cc4d65
case <case@casesandberg.com>
2016-09-04 20:15:31 -0700

    TwitterPicker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ export { default as PhotoshopPicker } from './components/photoshop/Photoshop'
 export { default as SketchPicker } from './components/sketch/Sketch'
 export { default as SliderPicker } from './components/slider/Slider'
 export { default as SwatchesPicker } from './components/swatches/Swatches'
+export { default as TwitterPicker } from './components/twitter/Twitter'
+
 export { default as CustomPicker } from './components/common/ColorWrap'
 
 export default from './components/chrome/Chrome'
