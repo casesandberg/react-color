690bf988e438eb6f8f4a3974dae59da16f61b706
Dwight Ware <dwightware4@users.noreply.github.com>
2016-09-22 16:03:56 -0700

    changed idx to i per request

--- src/components/twitter/Twitter.js
+++ src/components/twitter/Twitter.js
@@ -99,9 +99,9 @@ export class Twitter extends React.Component {
 
         <div style={ styles.body }>
 
-          { map(this.props.colors, (c, idx) => {
+          { map(this.props.colors, (c, i) => {
             return (
-              <Swatch key={idx} color={ c } hex={ c } style={ styles.swatch } onClick={ this.handleChange } />
+              <Swatch key={ i } color={ c } hex={ c } style={ styles.swatch } onClick={ this.handleChange } />
             )
           }) }
           <div style={ styles.hash }>#</div>
