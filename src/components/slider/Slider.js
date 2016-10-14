f077184fa06f31a7a17b1dd2d4d1a144d776621c
case <case@casesandberg.com>
2016-10-14 09:05:10 -0700

    Slider Component Cleanup + Snapshots

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,51 +1,38 @@
-'use strict' /* @flow */
-
 import React from 'react'
 import reactCSS from 'reactcss'
-import shallowCompare from 'react-addons-shallow-compare'
 
 import { ColorWrap, Hue } from '../common'
 import SliderSwatches from './SliderSwatches'
 import SliderPointer from './SliderPointer'
 
-export class Slider extends React.Component {
-  shouldComponentUpdate = shallowCompare.bind(this, this, arguments[0], arguments[1])
-
-  handleChange = (data: any) => {
-    this.props.onChange(data)
-  }
-
-  render(): any {
-    const styles = reactCSS({
-      'default': {
-        slider: {
-        },
-        hue: {
-          height: '12px',
-          position: 'relative',
-        },
-        Hue: {
-          radius: '2px',
-        },
+export const Slider = ({ hsl, onChange }) => {
+  const styles = reactCSS({
+    'default': {
+      hue: {
+        height: '12px',
+        position: 'relative',
       },
-    })
+      Hue: {
+        radius: '2px',
+      },
+    },
+  })
 
-    return (
-      <div style={ styles.slider } className="slider-picker">
-        <div style={ styles.hue }>
-          <Hue
-            style={ styles.Hue }
-            { ...this.props }
-            pointer={ SliderPointer }
-            onChange={ this.handleChange }
-          />
-        </div>
-        <div style={ styles.swatches }>
-          <SliderSwatches { ...this.props } onClick={ this.handleChange } />
-        </div>
+  return (
+    <div className="slider-picker">
+      <div style={ styles.hue }>
+        <Hue
+          style={ styles.Hue }
+          hsl={ hsl }
+          pointer={ SliderPointer }
+          onChange={ onChange }
+        />
       </div>
-    )
-  }
+      <div style={ styles.swatches }>
+        <SliderSwatches hsl={ hsl } onClick={ onChange } />
+      </div>
+    </div>
+  )
 }
 
 export default ColorWrap(Slider)
--- src/components/slider/SliderSwatches.js
+++ src/components/slider/SliderSwatches.js
@@ -1,89 +1,78 @@
-'use strict' /* @flow */
-
 import React from 'react'
 import reactCSS from 'reactcss'
-import shallowCompare from 'react-addons-shallow-compare'
 
 import SliderSwatch from './SliderSwatch'
 
-export class SliderSwatches extends React.Component {
-  shouldComponentUpdate = shallowCompare.bind(this, this, arguments[0], arguments[1])
-
-  handleClick = (data: any) => {
-    this.props.onClick(data)
-  }
-
-  render(): any {
-    const styles = reactCSS({
-      'default': {
-        swatches: {
-          marginTop: '20px',
-        },
-        swatch: {
-          boxSizing: 'border-box',
-          width: '20%',
-          paddingRight: '1px',
-          float: 'left',
-        },
-        clear: {
-          clear: 'both',
-        },
+export const SliderSwatches = ({ onClick, hsl }) => {
+  const styles = reactCSS({
+    'default': {
+      swatches: {
+        marginTop: '20px',
       },
-    })
+      swatch: {
+        boxSizing: 'border-box',
+        width: '20%',
+        paddingRight: '1px',
+        float: 'left',
+      },
+      clear: {
+        clear: 'both',
+      },
+    },
+  })
 
-    return (
-      <div style={ styles.swatches }>
-        <div style={ styles.swatch }>
-          <SliderSwatch
-            { ...this.props }
-            offset=".80"
-            active={ Math.round(this.props.hsl.l * 100) / 100 === 0.80
-              && Math.round(this.props.hsl.s * 100) / 100 === 0.50 }
-            onClick={ this.handleClick }
-            first
-          />
-        </div>
-        <div style={ styles.swatch }>
-          <SliderSwatch
-            { ...this.props }
-            offset=".65"
-            active={ Math.round(this.props.hsl.l * 100) / 100 === 0.65
-              && Math.round(this.props.hsl.s * 100) / 100 === 0.50 }
-            onClick={ this.handleClick }
-          />
-        </div>
-        <div style={ styles.swatch }>
-          <SliderSwatch
-            { ...this.props }
-            offset=".50"
-            active={ Math.round(this.props.hsl.l * 100) / 100 === 0.50
-               && Math.round(this.props.hsl.s * 100) / 100 === 0.50 }
-            onClick={ this.handleClick }
-          />
-        </div>
-        <div style={ styles.swatch }>
-          <SliderSwatch
-            { ...this.props }
-            offset=".35"
-            active={ Math.round(this.props.hsl.l * 100) / 100 === 0.35
-              && Math.round(this.props.hsl.s * 100) / 100 === 0.50 }
-            onClick={ this.handleClick }
-          />
-        </div>
-        <div style={ styles.swatch }>
-          <SliderSwatch
-            { ...this.props }
-            offset=".20"
-            active={ Math.round(this.props.hsl.l * 100) / 100 === 0.20
-              && Math.round(this.props.hsl.s * 100) / 100 === 0.50 }
-            onClick={ this.handleClick }
-            last
-          />
-        </div>
-        <div style={ styles.clear } />
+  return (
+    <div style={ styles.swatches }>
+      <div style={ styles.swatch }>
+        <SliderSwatch
+          hsl={ hsl }
+          offset=".80"
+          active={ Math.round(hsl.l * 100) / 100 === 0.80
+            && Math.round(hsl.s * 100) / 100 === 0.50 }
+          onClick={ onClick }
+          first
+        />
       </div>
-    )
-  }
+      <div style={ styles.swatch }>
+        <SliderSwatch
+          hsl={ hsl }
+          offset=".65"
+          active={ Math.round(hsl.l * 100) / 100 === 0.65
+            && Math.round(hsl.s * 100) / 100 === 0.50 }
+          onClick={ onClick }
+        />
+      </div>
+      <div style={ styles.swatch }>
+        <SliderSwatch
+          hsl={ hsl }
+          offset=".50"
+          active={ Math.round(hsl.l * 100) / 100 === 0.50
+             && Math.round(hsl.s * 100) / 100 === 0.50 }
+          onClick={ onClick }
+        />
+      </div>
+      <div style={ styles.swatch }>
+        <SliderSwatch
+          hsl={ hsl }
+          offset=".35"
+          active={ Math.round(hsl.l * 100) / 100 === 0.35
+            && Math.round(hsl.s * 100) / 100 === 0.50 }
+          onClick={ onClick }
+        />
+      </div>
+      <div style={ styles.swatch }>
+        <SliderSwatch
+          hsl={ hsl }
+          offset=".20"
+          active={ Math.round(hsl.l * 100) / 100 === 0.20
+            && Math.round(hsl.s * 100) / 100 === 0.50 }
+          onClick={ onClick }
+          last
+        />
+      </div>
+      <div style={ styles.clear } />
+    </div>
+  )
 }
 
 export default SliderSwatches
