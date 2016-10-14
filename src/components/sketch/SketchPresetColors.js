a5488f3d547367a7558ca2caa7b01438c0a782b1
case <case@casesandberg.com>
2016-10-14 08:33:09 -0700

    Sketch Components Cleanup + Snapshots

--- src/components/sketch/SketchPresetColors.js
+++ src/components/sketch/SketchPresetColors.js
@@ -1,67 +1,58 @@
-'use strict' /* @flow */
-
 import React from 'react'
 import reactCSS from 'reactcss'
 import map from 'lodash/map'
-import shallowCompare from 'react-addons-shallow-compare'
 
 import { Swatch } from '../common'
 
-export class SketchPresetColors extends React.Component {
-  shouldComponentUpdate = shallowCompare.bind(this, this, arguments[0], arguments[1])
+export const SketchPresetColors = ({ colors, onClick }) => {
+  const styles = reactCSS({
+    'default': {
+      colors: {
+        margin: '0 -10px',
+        padding: '10px 0 0 10px',
+        borderTop: '1px solid #eee',
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      swatchWrap: {
+        width: '16px',
+        height: '16px',
+        margin: '0 10px 10px 0',
+      },
+      swatch: {
+        borderRadius: '3px',
+        boxShadow: 'inset 0 0 0 1px rgba(0,0,0,.15)',
+      },
+    },
+    'no-presets': {
+      colors: {
+        display: 'none',
+      },
+    },
+  }, {
+    'no-presets': !colors || !colors.length,
+  })
 
-  handleClick = (hex: any) => {
-    this.props.onClick({
+  const handleClick = (hex) => {
+    onClick({
       hex,
       source: 'hex',
     })
   }
 
-  render(): any {
-    const styles = reactCSS({
-      'default': {
-        colors: {
-          margin: '0 -10px',
-          padding: '10px 0 0 10px',
-          borderTop: '1px solid #eee',
-          display: 'flex',
-          flexWrap: 'wrap',
-        },
-        swatchWrap: {
-          width: '16px',
-          height: '16px',
-          margin: '0 10px 10px 0',
-        },
-        swatch: {
-          borderRadius: '3px',
-          boxShadow: 'inset 0 0 0 1px rgba(0,0,0,.15)',
-        },
-      },
-      'no-presets': {
-        colors: {
-          display: 'none',
-        },
-      },
-    }, {
-      'no-presets': !this.props.colors || !this.props.colors.length,
-    })
-
-    return (
-      <div style={ styles.colors } className="flexbox-fix">
-        { map(this.props.colors, (c) => {
-          return (
-            <div style={ styles.swatchWrap } key={ c }>
-              <Swatch
-                color={ c }
-                style={ styles.swatch }
-                onClick={ this.handleClick }
-              />
-            </div>
-          )
-        }) }
-      </div>
-    )
-  }
+  return (
+    <div style={ styles.colors } className="flexbox-fix">
+      { map(colors, (c) => (
+        <div key={ c } style={ styles.swatchWrap }>
+          <Swatch
+            color={ c }
+            style={ styles.swatch }
+            onClick={ handleClick }
+          />
+        </div>
+      )) }
+    </div>
+  )
 }
 
 export default SketchPresetColors
