88daec77eb64334a82680714cdc9e2f0ee8b6822
case <case@casesandberg.com>
2017-08-15 13:44:25 -0700

    V1 of Storybook Report

---
+++ .storybook/config.js
@@ -0,0 +1,18 @@
+import { configure, addDecorator } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+
+import centered from '@storybook/addon-centered';
+import { withKnobs } from '@storybook/addon-knobs';
+
+setOptions({
+  name: 'React Color',
+  downPanelInRight: true,
+})
+
+addDecorator(centered);
+addDecorator(withKnobs);
+
+const req = require.context('../src/components', true, /\.?story\.js$/)
+const loadStories = () => req.keys().forEach((filename) => req(filename))
+
+configure(loadStories, module);
