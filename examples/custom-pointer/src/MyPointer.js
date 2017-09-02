3890e552baccb9a53449f47ce7e74553d8489668
case <case@casesandberg.com>
2017-09-02 14:32:42 -0700

    Custom Pointer Example

---
+++ examples/custom-pointer/src/MyPointer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const MyPointer = () => {
+  return (
+    <div
+      style={{
+        transform: 'translate(-50%, -10px)',
+        cursor: 'pointer',
+        fontSize: 32,
+      }}
+    >
+      🔥
+    </div>
+  )
+}
+
+export default MyPointer
