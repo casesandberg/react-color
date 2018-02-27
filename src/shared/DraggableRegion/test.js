7011b12fafb6cdedc0cbf62a30d18e33ccdf088a
case <case@casesandberg.com>
2018-02-27 12:12:36 -1000

    Positioned Div Test

--- src/shared/DraggableRegion/test.js
+++ src/shared/DraggableRegion/test.js
@@ -32,3 +32,27 @@ test('returns location on click', () => {
     y: 0.2,
   })
 })
+
+test('returns location on click with positioned div', () => {
+  const handleChange = jest.fn()
+  const container = shallow((
+    <DraggableRegion onChange={ handleChange } />
+  ))
+  container.setState({
+    top: 100,
+    left: 100,
+    width: 200,
+    height: 100,
+  })
+  container.find('div').simulate('mousedown', createEvent({ pageX: 120, pageY: 120 }))
+  expect(handleChange).toHaveBeenCalledWith({
+    top: 100,
+    left: 100,
+    width: 200,
+    height: 100,
+    insideLeft: 20,
+    insideTop: 20,
+    x: 0.1,
+    y: 0.2,
+  })
+})
