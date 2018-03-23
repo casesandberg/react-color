6abb87ea48abab53a7b9a1aaea66bf8245e9bead
case <case@casesandberg.com>
2018-03-23 10:48:04 -0700

    Fix Down Arrow Naming

--- packages/keys/index.js
+++ packages/keys/index.js
@@ -10,5 +10,5 @@ export const SPACEBAR = 32
 export const LEFT_ARROW = 37
 export const UP_ARROW = 38
 export const RIGHT_ARROW = 39
-export const BOTTOM_ARROW = 40
+export const DOWN_ARROW = 40
 export const CMD = 91
