f87bb865d01e725565b934cdca643281ec50f9c7
case <case@casesandberg.com>
2018-04-11 19:12:07 -0700

    Checkerboard + Tests

---
+++ packages/core/Checkerboard/story.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Checkerboard from './Checkerboard'
+
+storiesOf('core/Checkerboard', module)
+  .add('default', () => {
+    return (
+      <div style={{ width: 64, height: 64, display: 'flex' }}>
+        <Checkerboard />
+      </div>
+    )
+  })
+  .add('custom size, color', () => {
+    return (
+      <div style={{ width: 64, height: 64, display: 'flex', backgroundColor: '#333' }}>
+        <Checkerboard size={16} color="teal" />
+      </div>
+    )
+  })
