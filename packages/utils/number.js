3c18225e7c9f8558c3494464f37ec3cd19b5b4ad
case <case@casesandberg.com>
2018-04-15 10:50:18 -0700

    Type Secondary Packages

--- packages/utils/number.js
+++ packages/utils/number.js
@@ -1,3 +1,11 @@
-export const clamp = ({ min = 0, max, value }) => {
+// @flow
+
+type Clamp = {
+  min?: number,
+  max: number,
+  value: number,
+}
+
+export const clamp = ({ min = 0, max, value }: Clamp): number => {
   return Math.max(min, Math.min(value, max))
 }
--- packages/utils/number.test.js
+++ packages/utils/number.test.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { clamp } from './number'
 
 describe('clamp', () => {
